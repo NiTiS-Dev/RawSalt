774da8c1bffe4d35fe0f577030d1f3139dd9b1ab
NiTiSon <NiTiSon@users.noreply.github.com>
2023-08-27 14:38:04 +0300

    add: annotation (may use in future)

---
+++ src/RawSalt.Engine/ExportAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace RawSalt.Engine;
+
+/// <summary>
+/// Indicate field or property as visible on editor
+/// </summary>
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+public class ExportAttribute : Attribute
+{
+
+}

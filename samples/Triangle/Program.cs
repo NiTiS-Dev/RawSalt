43de6e9d3a53e2f5e97a67d20f034651fd15e3a2
NiTiSon <NiTiSon@users.noreply.github.com>
2023-11-02 00:45:47 +0300

    New sample

--- samples/Triangle/Program.cs
+++ samples/Triangle/Program.cs
@@ -1,40 +1,27 @@
-﻿using System;
-using System.Runtime.CompilerServices;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Drawing;
-using SixLabors.ImageSharp.Drawing.Processing;
-using SixLabors.ImageSharp.PixelFormats;
-using SixLabors.ImageSharp.Processing;
+﻿using RawSalt;
+using RawSalt.Graphics;
+using RawSalt.Native.Vulkan;
+using RawSalt.Native.Windows;
+using System;
 
 namespace Triangle;
 
-internal class Program
+public class Program : Application
 {
+	public Program(RenderOptions renderOptions) : base(renderOptions)
+	{
+	}
+
 	static void Main(string[] args)
 	{
-		const int H = 500;
-		const int W = 500;
-
-		using Image<Rgba32> img = new Image<Rgba32>(H, W);
-
-		Random rnd = new();
-		img.ProcessPixelRows( rows =>
+		try
 		{
-			for (int y = 0; y < H; y++)
-			{
-				var span = rows.GetRowSpan(y);
-				for (int x = 0; x < W; x++)
-				{
-					Rgba32 colour = default;
-					colour.R = 144;
-					colour.G = 144;
-					colour.B = 144;
-					colour.A = 255;
-					span[x] = colour;
-				}
-			}
-		});
-
-		img.SaveAsPng("C:/Desktop/img.png");
+			Program app = new(new RenderOptions(false, 60));
+			app.Run();
+		}
+		catch (Exception e)
+		{
+			Console.Error.WriteLine(e);
+		}
 	}
 }
